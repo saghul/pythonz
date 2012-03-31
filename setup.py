eb9f559aaeef39f94c92e8c9c69c378e3bbb3133
saghul <saghul@gmail.com>
2012-03-31 13:51:19 +0200

    Adapted setup.py file

--- setup.py
+++ setup.py
@@ -1,35 +1,34 @@
+
 import os
-from setuptools import setup, find_packages
-from pythonbrew.define import VERSION
+from distutils import setup
 
-README = os.path.join(os.path.dirname(__file__),'PKG-INFO')
-long_description = open(README).read() + "\n"
 
-setup(name='pythonbrew',
-      version=VERSION,
-      description="Manage python installations in your $HOME",
-      long_description=long_description,
-      classifiers=[
+def find_packages(toplevel):
+    return [directory.replace(os.path.sep, '.') for directory, subdirs, files in os.walk(toplevel) if '__init__.py' in files]
+
+setup(name='pythonz',
+      version              = 'dev',
+      description          = 'Manage python installations in your system',
+      long_description     = open('README.rst').read(),
+      author               = 'utahta',
+      author_email         = 'labs.ninxit@gmail.com',
+      maintainer           = 'saghul',
+      maintainer_email     = 'saghul@gmail.com',
+      url                  = 'https://github.com/saghul/pythonz',
+      license              = 'MIT',
+      packages             = find_packages('pythonz'),
+      include_package_data = True,
+      entry_points         = dict(console_scripts=['pythonz_install=pythonz.installer:install_pythonz']),
+      zip_safe             = False,
+      classifiers          = [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Build Tools',
-        'Programming Language :: Python :: 2.4',
-        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
-      ],
-      keywords='pythonbrew pip easy_install distutils setuptools virtualenv',
-      author='utahta',
-      author_email='labs.ninxit@gmail.com',
-      url='https://github.com/utahta/pythonbrew',
-      license='MIT',
-      packages=find_packages(),
-      include_package_data=True,
-      entry_points=dict(console_scripts=['pythonbrew_install=pythonbrew.installer:install_pythonbrew']),
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      zip_safe=False)
+      ])
+
