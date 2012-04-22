f8a6cc70c3c96c8c029edc7154c8e587bd1f1e23
saghul <saghul@gmail.com>
2012-04-22 10:52:55 +0200

    Don't clean the build directory by default, that will remove the debug symbols
    Fixes issue #4

--- pythonz/commands/cleanup.py
+++ pythonz/commands/cleanup.py
@@ -11,8 +11,19 @@ class CleanupCommand(Command):
     usage = "%prog"
     summary = "Remove stale source folders and archives"
     
+    def __init__(self):
+        super(CleanupCommand, self).__init__()
+        self.parser.add_option(
+            '-a', '--all',
+            dest='clean_all',
+            action='store_true',
+            default=False,
+            help='Clean all, including the build directory. Note that debug symbols will be gone too!'
+        )
+
     def run_command(self, options, args):
-        self._cleanup(PATH_BUILD)
+        if options.clean_all:
+            self._cleanup(PATH_BUILD)
         self._cleanup(PATH_DISTS)
         
     def _cleanup(self, root):
