a7cb4a26d9b237609b6a4ab4af931fa7a96eca82
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-04-07 15:21:31 +0200

    Added support for CPython 2.7.4

--- pythonz/installer/pythoninstaller.py
+++ pythonz/installer/pythoninstaller.py
@@ -98,7 +98,7 @@ class CPythonInstaller(Installer):
     supported_versions = ['2.4', '2.4.1', '2.4.2', '2.4.3', '2.4.4', '2.4.5', '2.4.6',
                           '2.5', '2.5.1', '2.5.2', '2.5.3', '2.5.4', '2.5.5', '2.5.6',
                           '2.6', '2.6.1', '2.6.2', '2.6.3', '2.6.4', '2.6.5', '2.6.6', '2.6.7', '2.6.8',
-                          '2.7', '2.7.1', '2.7.2', '2.7.3',
+                          '2.7', '2.7.1', '2.7.2', '2.7.3', '2.7.4',
                           '3.0', '3.0.1',
                           '3.1', '3.1.1', '3.1.2', '3.1.3', '3.1.4', '3.1.5',
                           '3.2', '3.2.1', '3.2.2', '3.2.3',
@@ -290,7 +290,8 @@ class CPythonInstaller(Installer):
                                                   {'locale.py.ed': 'Lib/locale.py'}])
         elif is_python27(version):
             PATH_PATCHES_OSX_PYTHON27 = os.path.join(PATH_PATCHES_OSX, "python27")
-            self._append_patch(PATH_PATCHES_OSX_PYTHON27, ['patch-Modules-posixmodule.diff'])
+            if version < '2.7.4':
+                self._append_patch(PATH_PATCHES_OSX_PYTHON27, ['patch-Modules-posixmodule.diff'])
 
     def patch(self):
         if sys.platform == "darwin":
