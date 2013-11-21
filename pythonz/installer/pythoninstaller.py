188ca6d957805d6770c1a578faf10baa4927655a
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-11-21 11:06:03 +0100

    Added support for CPython 3.3.3

--- pythonz/installer/pythoninstaller.py
+++ pythonz/installer/pythoninstaller.py
@@ -101,7 +101,7 @@ class CPythonInstaller(Installer):
                           '3.0', '3.0.1',
                           '3.1', '3.1.1', '3.1.2', '3.1.3', '3.1.4', '3.1.5',
                           '3.2', '3.2.1', '3.2.2', '3.2.3', '3.2.4', '3.2.5',
-                          '3.3.0', '3.3.1', '3.3.2']
+                          '3.3.0', '3.3.1', '3.3.2', '3.3.3']
 
     def __init__(self, version, options):
         super(CPythonInstaller, self).__init__(version, options)
