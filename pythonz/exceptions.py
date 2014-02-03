c5df721845307e78fb2e68210716ef67864edf30
Saúl Ibarra Corretgé <saghul@gmail.com>
2014-02-03 23:20:53 +0100

    Remove unused exceptions

--- pythonz/exceptions.py
+++ pythonz/exceptions.py
@@ -1,12 +1,6 @@
 
-class BuildingException(Exception):
-    """General exception during building"""
-
 class ShellCommandException(Exception):
     """General exception during shell command"""
-
-class AlreadyInstalledException(Exception):
-    """General exception during installing"""
 
 class DownloadError(Exception):
     """Exception during download"""
