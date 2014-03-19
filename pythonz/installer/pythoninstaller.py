2673fca157c1372193ac1c65ae8fb9cbf1fbcd09
Saúl Ibarra Corretgé <saghul@gmail.com>
2014-03-19 01:31:00 +0100

    Added support for stackless 3.2.5 and 3.3.5

--- pythonz/installer/pythoninstaller.py
+++ pythonz/installer/pythoninstaller.py
@@ -374,7 +374,8 @@ class StacklessInstaller(CPythonInstaller):
     supported_versions = ['2.6.5',
                           '2.7.2',
                           '3.1.3',
-                          '3.2.2']
+                          '3.2.2', '3.2.5',
+                          '3.3.5']
 
     @classmethod
     def get_version_url(cls, version):
