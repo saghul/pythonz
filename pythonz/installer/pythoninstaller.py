2702c2fa6df0606f3435ac42fd4e8a152fa0738c
Saúl Ibarra Corretgé <saghul@gmail.com>
2014-05-19 17:19:24 +0200

    Add support for CPython 3.4.1

--- pythonz/installer/pythoninstaller.py
+++ pythonz/installer/pythoninstaller.py
@@ -102,7 +102,7 @@ class CPythonInstaller(Installer):
                           '3.1', '3.1.1', '3.1.2', '3.1.3', '3.1.4', '3.1.5',
                           '3.2', '3.2.1', '3.2.2', '3.2.3', '3.2.4', '3.2.5',
                           '3.3.0', '3.3.1', '3.3.2', '3.3.3', '3.3.4', '3.3.5',
-                          '3.4.0']
+                          '3.4.0', '3.4.1']
 
     def __init__(self, version, options):
         super(CPythonInstaller, self).__init__(version, options)
