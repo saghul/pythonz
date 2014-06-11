00435c0e504810ce1a25a095ceb564a067534a48
Saúl Ibarra Corretgé <saghul@gmail.com>
2014-06-11 10:01:59 +0200

    Add support for PyPy 2.3.1

--- pythonz/installer/pythoninstaller.py
+++ pythonz/installer/pythoninstaller.py
@@ -381,7 +381,7 @@ class PyPyInstaller(Installer):
                           '2.0', '2.0.1', '2.0.2',
                           '2.1',
                           '2.2', '2.2.1',
-                          '2.3']
+                          '2.3', '2.3.1']
 
     @classmethod
     def get_version_url(cls, version):
